55867f9158c384988aca1c1c5887f3953990532e
Ky15012004 <nguyenphuocky2021qt@gmail.com>
2025-02-21 20:07:04 +0700

    commit Bai6

---
+++ Bai6/Bai6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n_224;
+            List<int> soNguyen_224 = new List<int>();
+            Console.WriteLine("Nhap n: ");
+            n_224 = int.Parse(Console.ReadLine());
+            int i = 0;
+            while (n_224 > 0)
+            {
+               
+                soNguyen_224.Add(n_224 % 10);
+                n_224 = n_224 / 10;
+                i++;
+            }
+
+            Console.WriteLine("So lon nhat la: {0}", soNguyen_224.Max());
+            Console.ReadKey();
+        }
+    }
+}

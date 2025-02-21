32b70ccdd342aa3df677303f7299be5de35f0b48
Ky15012004 <nguyenphuocky2021qt@gmail.com>
2025-02-21 20:45:08 +0700

    commit bai7

---
+++ Bai7/Bai7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n_224;
+            List<int> soNguyen_224 = new List<int>();
+            Boolean chk= true;
+            Console.WriteLine("Nhap n: ");
+            n_224 = int.Parse(Console.ReadLine());
+            int number = n_224;
+            int i = 0;
+            while (n_224 > 0)
+            {
+                soNguyen_224.Add(n_224 % 10);
+                n_224 = n_224 / 10;
+                i++;
+            }
+            
+            for(int j=0; j<i-1; j++)
+            {
+                if (soNguyen_224[j] < soNguyen_224[j + 1])
+                {
+                    chk = false;
+                    break;
+                }                         
+            }
+
+            if (chk == false)
+                Console.WriteLine("so nguyen duong {0} khong phai la so giam dan tu trai qua phai", number);
+            else
+                Console.WriteLine("so nguyen duong {0} la so giam dan tu trai qua phai", number);
+            
+
+            Console.ReadKey();
+            
+        }
+    }
+}

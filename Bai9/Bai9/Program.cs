fcb11901272f7ca78fed46976693f2234960c2e2
Ky15012004 <nguyenphuocky2021qt@gmail.com>
2025-02-21 20:12:42 +0700

    commit Bai9

---
+++ Bai9/Bai9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c, max;
+
+            Console.WriteLine("Nhap a: ");
+            a = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap b: ");
+            b = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap c: ");
+            c = float.Parse(Console.ReadLine());
+
+            max = a;
+
+            if (max < b)
+                max = b;
+            if (max < c)
+                max = c;
+
+            Console.WriteLine("So lon nhat la: {0}", max);
+            Console.ReadKey();
+        }
+    }
+}

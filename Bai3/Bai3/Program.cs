a94baed5ebc86500dc2b37403a061cb9f04a6c9f
Ky15012004 <nguyenphuocky2021qt@gmail.com>
2025-02-21 19:16:21 +0700

    commit Bai 3

---
+++ Bai3/Bai3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n_224, result_224 = 0;
+            Console.WriteLine("Nhap n: ");
+            n_224 = int.Parse(Console.ReadLine());
+
+            for(int i=1; i<=n_224; i++)
+            {
+                if (n_224 % i == 0)
+                    result_224 += i;
+            }
+
+            Console.WriteLine("ket qua la: {0}", result_224);
+            Console.ReadKey();
+        }
+    }
+}

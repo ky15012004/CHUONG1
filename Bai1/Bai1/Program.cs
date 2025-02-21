8af2bb96619a94b5ff887aa0c070c237eee4a4f5
Ky15012004 <nguyenphuocky2021qt@gmail.com>
2025-02-21 19:09:44 +0700

    commit bai 1

---
+++ Bai1/Bai1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n_224, result_224 = 0;
+            Console.WriteLine("Nhap n: ");
+            n_224 = int.Parse(Console.ReadLine());
+
+            for(int i=1; i<=n_224; i++)
+            {
+                result_224 += i;
+            }
+
+            Console.WriteLine("Tong la: {0}", result_224);
+            Console.ReadKey();
+        }
+    }
+}

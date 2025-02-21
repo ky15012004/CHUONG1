dad35e474743115218a5b0b147ba8ea06907f3e7
Ky15012004 <nguyenphuocky2021qt@gmail.com>
2025-02-21 20:04:25 +0700

    commit Bai 5

---
+++ Bai5/Bai5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n_224, sum_224=0, k_224=0;
+            Console.WriteLine("Nhap n: ");
+            n_224 = int.Parse(Console.ReadLine());
+
+
+            while(sum_224 + (k_224 + 1) < n_224)
+            {
+                k_224++;
+                sum_224 += k_224;
+            }
+
+            Console.WriteLine("Gia tri k lon nhat la: {0}"  ,k_224);
+            Console.ReadKey();
+        }
+    }
+}
